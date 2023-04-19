48c13d257c9b305665b160e2466d738c9e58f85a
zsmoore <zsmoore@zachary-moore.com>
2023-04-19 15:19:54 -0600

    Add a navbar on detail pages

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,9 +1,11 @@
 import { getAll, getSinglePost, PageResult, Post } from "@component/lib/notion";
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import { Tags } from "@component/components/tags";
 import Footer from "@component/components/footer";
 import { ShareBar } from "@component/components/share";
 import Head from "next/head";
+import Link from "next/link";
 
 interface PostResultProps {
   pageResult: PageResult,
@@ -11,27 +13,32 @@ interface PostResultProps {
 
 export default function PostResult(props: PostResultProps) {
   return (
-    <>
+    <div className="w-screen bg-white dark:bg-black">
       <Head>
         <title>{props.pageResult.post.title}</title>
         <meta name="keywords" content={props.pageResult.post.tags.join(', ')}/>
         <meta name="description" content={`On This Day in History: ${props.pageResult.post.title}`}/>
       </Head>
-      <div className="w-screen bg-white dark:bg-black">
-        <div className="justify-center items-center">
-          <div className="container mx-auto px-8 max-w-3xl justify-center items-center">
-            <div className="prose dark:prose-invert lg:prose-lg mx-auto my-8">
-              <div className="py-4">
-                <Tags tags={props.pageResult.post.tags} />
-              </div>
-              <ReactMarkdown>{props.pageResult.markdown}</ReactMarkdown>
-            </div>
-            <ShareBar slug={props.pageResult.post.slug} />
-          </div>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
+          <Link href="/" className="flex-items-center">
+            <span className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 bg-clip-text font-extrabold text-transparent text-md sm:text-lg leading-tight sm:leading-tight lg:leading-relaxed">On This Day in History</span>
+          </Link>
         </div>
-        <Footer />
+      </nav>
+      <div className="justify-center items-center">
+        <div className="container mx-auto px-8 max-w-3xl justify-center items-center">
+          <div className="prose dark:prose-invert lg:prose-lg mx-auto my-8">
+            <div className="py-4">
+              <Tags tags={props.pageResult.post.tags} />
+            </div>
+            <ReactMarkdown>{props.pageResult.markdown}</ReactMarkdown>
+          </div>
+          <ShareBar slug={props.pageResult.post.slug} />
+        </div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
