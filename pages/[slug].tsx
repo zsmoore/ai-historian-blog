a9ac7c67965fbb601bd621a7b307dcc98e3a44a0
zsmoore <zsmoore@zachary-moore.com>
2023-04-19 11:16:31 -0600

    Add head to detail pages

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,11 +1,10 @@
 import { getAll, getSinglePost, PageResult, Post } from "@component/lib/notion";
 import ReactMarkdown from 'react-markdown';
-import { Inter, Share } from 'next/font/google'
 import { Tags } from "@component/components/tags";
 import Footer from "@component/components/footer";
 import { ShareBar } from "@component/components/share";
-
-const inter = Inter({ subsets: ['latin'] });
+import { Main } from "next/document";
+import Head from "next/head";
 
 interface PostResultProps {
   pageResult: PageResult,
@@ -13,20 +12,27 @@ interface PostResultProps {
 
 export default function PostResult(props: PostResultProps) {
   return (
-    <div className="w-screen bg-white dark:bg-black">
-      <div className="justify-center items-center">
-        <div className="container mx-auto px-8 max-w-3xl justify-center items-center">
-          <div className="prose dark:prose-invert lg:prose-lg mx-auto my-8">
-            <div className="py-4">
-              <Tags tags={props.pageResult.post.tags} />
+    <>
+      <Head>
+        <title>{props.pageResult.post.title}</title>
+        <meta name="keywords" content={props.pageResult.post.tags.join(', ')}/>
+        <meta name="description" content={`On This Day in History: ${props.pageResult.post.title}`}/>
+      </Head>
+      <div className="w-screen bg-white dark:bg-black">
+        <div className="justify-center items-center">
+          <div className="container mx-auto px-8 max-w-3xl justify-center items-center">
+            <div className="prose dark:prose-invert lg:prose-lg mx-auto my-8">
+              <div className="py-4">
+                <Tags tags={props.pageResult.post.tags} />
+              </div>
+              <ReactMarkdown>{props.pageResult.markdown}</ReactMarkdown>
             </div>
-            <ReactMarkdown>{props.pageResult.markdown}</ReactMarkdown>
+            <ShareBar slug={props.pageResult.post.slug} />
           </div>
-          <ShareBar slug={props.pageResult.post.slug} />
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
 
